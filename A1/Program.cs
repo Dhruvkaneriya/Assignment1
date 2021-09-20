1adf74033e7b945366e46efe1ecde41d4072bc5e
Dhruvkaneriya <dhruvkaneriya5@gmail.com>
2021-09-20 19:21:02 -0400

    first commit

---
+++ A1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A1
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the number of cards to pick: ");
+			string line = Console.ReadLine();
+			if (int.TryParse(line, out int numCards))
+			{
+				foreach (var card in CardPicker.PickSomeCards(numCards))
+				{
+					Console.WriteLine(card);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Please enter a valid number.");
+			}
+		}
+	}
+}
